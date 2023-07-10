c7c723a9c59a65affcaeaa41760e016572b354a5
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-07-11 00:16:56 +0300

    added services

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -7,6 +8,14 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
   constructor(private http: HttpClient) {}
 
-  getThemes() {}
-  getPosts() {}
+  getThemes() {
+    const { appUrl } = environment;
+    return this.http.get(`${appUrl}/themes`);
+  }
+  getPosts(limit?: number) {
+    const { appUrl } = environment;
+    const limitFilter = limit ? `?limit=${limit}` : '';
+
+    return this.http.get(`${appUrl}/posts${limitFilter}`);
+  }
 }
