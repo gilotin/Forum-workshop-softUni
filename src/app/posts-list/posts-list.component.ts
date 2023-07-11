58bbe5cfb0b281a8f7d40fa7c61a3a4bce223e5f
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-07-11 12:10:32 +0300

    added spinner/loading

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -9,11 +9,19 @@ import { Post } from '../types/post';
 })
 export class PostsListComponent implements OnInit {
   postList: Post[] = [];
+  isLoading: boolean = true;
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService.getPosts(5).subscribe((posts) => {
-      this.postList = posts;
+    this.apiService.getPosts(5).subscribe({
+      next: (posts) => {
+        this.postList = posts;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error(`Error: ${error}`);
+        this.isLoading = false;
+      },
     });
   }
 }
